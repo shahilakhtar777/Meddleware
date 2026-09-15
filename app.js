2d19544ad0c34b788c3b99ea1564596dcd5bff75
Shahil Akhatar <shahilakhatar83@gmail.com>
2026-09-15 23:19:13 +0530

    next();

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const express =require("express");
-const app = express();
+const app = express(); 
+
+app.use((req, res, next)=>{
+    console.log("Hi, I am 1st middleware");
+     next();
+   
+});
+
+app.use((req, res, next)=>{
+    console.log("Hi, I am 2nd middleware");
+    next();
+    
+});
 
 app.get("/",(req,res)=>{
-    res.send();
+    res.send("HI, I am root." );
 });
+
+app.get("/random", (req,res)=>{
+    res.send("this is random page");
+})
 
 app.listen(8080,()=>{
     console.log("server listening to port 8080");
