c4b59d1cab5e7cee9252d5c5fecf64efbf6b4ecc
Shahil Akhatar <shahilakhatar83@gmail.com>
2026-09-15 23:50:49 +0530

    logger, morgon

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 const express =require("express");
 const app = express(); 
 
-app.use((req, res, next)=>{
-    console.log("Hi, I am 1st middleware");
-     next();
+// app.use((req, res, next)=>{
+//     console.log("Hi, I am 1st middleware");
+//      next();
    
-});
+// });
  
-app.use((req, res, next)=>{
-    console.log("Hi, I am 2nd middleware");
-    next();
+// app.use((req, res, next)=>{
+//     console.log("Hi, I am 2nd middleware");
+//     next();
     
-});
+// });
+    app.use((req,res, next)=>{
+        req.time = Date.now();
+        console.log(req.method,req.hostname, req.path, req.time);
+        next();
+    });
+
 
 app.get("/",(req,res)=>{
     res.send("HI, I am root." );
@@ -21,6 +27,12 @@ app.get("/random", (req,res)=>{
     res.send("this is random page");
 })
 
+  app.use((req,res, next)=>{
+        req.time = Date.now();
+        console.log(req.method,req.hostname, req.path, req.time);
+        next();
+    });
+    
 app.listen(8080,()=>{
     console.log("server listening to port 8080");
 });  
