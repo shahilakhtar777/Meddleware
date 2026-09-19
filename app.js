cbaf6612fb196bd1b955d6018815dd67576fe3ba
Shahil Akhatar <shahilakhatar83@gmail.com>
2026-09-19 22:23:36 +0530

    ERROR Middleware

--- app.js
+++ app.js
@@ -40,7 +40,12 @@ const app = express();
 
         app.use((err, req,res, next) =>{
             console.log("------ERROR-----");
-            next();   
+            next(err);   
+        });
+
+              app.use((err, req,res, next) =>{
+            console.log("------ERROR Middleware-----");
+            next(err);   
         });
 
     app.use((req, res, next)=>{
